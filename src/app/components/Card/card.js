ef1697cf5f712143777e9c90597a7fb178f3e899
singh-ritu <your-email@example.com>
2025-06-14 13:35:55 +0530

    fix: altered product grid css

--- src/app/components/Card/card.js
+++ src/app/components/Card/card.js
@@ -8,26 +8,30 @@ function Card({ product }) {
   const [clicked, setClicked] = useState(false);
   return (
     <div key={product.id} className={styles.product}>
-      <Image
-        src={product.image}
-        alt="image"
-        className={styles.image}
-        width={200}
-        height={300}
-      />
-
-      <div className={styles.details}>
-        <div>
-          <p>{product.title}</p>
-          <p>{product.category}</p>
-        </div>
+      <div>
         <Image
-          src={clicked ? heartFull : heart}
-          alt="heart"
-          width={24}
-          height={24}
-          onClick={() => setClicked(!clicked)}
+          src={product.image}
+          alt="image"
+          className={styles.image}
+          width={300}
+          height={400}
         />
+      </div>
+      <div className={styles.details}>
+        <p className={styles.title}>{product.title}</p>
+        <div className={styles.subdetails}>
+          <p className={styles.category}>{product.category}</p>
+
+          <div>
+            <Image
+              src={clicked ? heartFull : heart}
+              alt="heart"
+              width={24}
+              height={24}
+              onClick={() => setClicked(!clicked)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
